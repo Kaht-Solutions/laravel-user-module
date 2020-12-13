96da670ed012bab5347eb5f4b8b6df6c3fb61741
Mehran NikNafs <mehran.niknafs@gmail.com>
2020-12-13 18:22:48 +0330

    Fix function names based on psr-2

--- Http/Controllers/PermissionController.php
+++ Http/Controllers/PermissionController.php
@@ -18,7 +18,7 @@ use \Modules\Location\Models\Province;
 class PermissionController extends Controller
 {
 
-    public function Index()
+    public function index()
     {
 
         $permissions = Permission::all();
@@ -26,27 +26,27 @@ class PermissionController extends Controller
         return view('user::' . env("ADMIN_THEME") . '.permission.index')->with('permissions', $permissions);
     }
 
-    public function Show()
+    public function show()
     {
         $permission = Permission::find(Request::get('id'));
 
         return view('user::' . env("ADMIN_THEME") . '.permission.show')->with('permission', $permission);
-
     }
 
-    public function Create()
+    public function create()
     {
         $provinces = Province::all();
         return view('user::' . env("ADMIN_THEME") . '.permission.create', ['provinces' => $provinces]);
     }
 
-    public function Store(Request $request)
+    public function store(Request $request)
     {
 
         $data = $request->all();
         $valid = Validator::make($data, [
-            'name' => 'required|max:255|unique:usermodule_permissions',
-            'display_name' => 'required|max:255|unique:usermodule_permissions']);
+            'name' => 'required|max:255|unique:usermodule_permissions,name',
+            'display_name' => 'required|max:255|unique:usermodule_permissions,display_name'
+        ]);
 
         if ($valid->fails()) {
             return Redirect::back()->withErrors($valid)->withInput();
@@ -60,7 +60,7 @@ class PermissionController extends Controller
         return Redirect::back()->withErrors(trans('user::messages.done'));
     }
 
-    public function Edit(Request $request)
+    public function edit(Request $request)
     {
         $id = $request->input('id');
         $permission = Permission::find($id);
@@ -68,13 +68,14 @@ class PermissionController extends Controller
         return view('user::' . env("ADMIN_THEME") . '.permission.edit')->with('permission', $permission)->with('provinces', $provinces);
     }
 
-    public function Update(Request $request)
+    public function update(Request $request)
     {
 
         $data = $request->all();
         $valid = Validator::make($data, [
             'name' => 'required|max:255|unique:usermodule_permissions,name,' . $data['id'],
-            'display_name' => 'required|max:255|unique:usermodule_permissions,display_name,' . $data['id']]);
+            'display_name' => 'required|max:255|unique:usermodule_permissions,display_name,' . $data['id']
+        ]);
 
         if ($valid->fails()) {
             return Redirect::back()->withErrors($valid)->withInput();
@@ -88,10 +89,9 @@ class PermissionController extends Controller
         $permission->save();
 
         return Redirect::back()->withErrors(trans('user::messages.done'));
-
     }
 
-    public function Destroy(Request $request)
+    public function destroy(Request $request)
     {
         $id = $request->input('id');
         $permission = Permission::find($id);
@@ -99,5 +99,4 @@ class PermissionController extends Controller
 
         return Redirect::back()->withErrors(trans('user::messages.done'));
     }
-
 }
